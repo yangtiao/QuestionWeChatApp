77f00116c4a7d2525f39db58fc51252a2922611f
yeung <282701107@qq.com>
2019-12-07 15:14:21 +0800

    1.0

--- WeChatApp/pages/my/index.js
+++ WeChatApp/pages/my/index.js
@@ -5,7 +5,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    userInfo:{}
+    userInfo:{},
+    disabled: false
   },
 
   /**
@@ -33,6 +34,44 @@ Page({
       title: '关于',
       content: '华特教育在线学习小程序',
       showCancel: false
+    })
+  },
+  login(){
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              // 可以将 res 发送给后台解码出 unionId
+              this.userInfo = res.userInfo
+              this.setData({
+                userInfo: this.userInfo,
+                disabled: false
+              })
+
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+              }
+            }
+          })
+        }
+      }
+    })
+  },
+  logout(){
+    this.setData({
+      userInfo: null,
+      disabled: true
     })
   }
 })
